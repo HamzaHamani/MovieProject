16893667ed72115d36d3d6f5c5e4455216b9a6bb
HamzaHamani <hamzahamani2004@gmail.com>
2025-06-15 12:44:04 +0100

    fixing small error for deployment

--- app/(normal nav)/bookmarks/page.tsx
+++ app/(normal nav)/bookmarks/page.tsx
@@ -7,17 +7,15 @@ export const metadata: Metadata = {
   title: "Bookmarks",
 };
 export default async function Saved({}) {
-  const data = await getMoviesBook("2f97ccca-06f1-4d1c-b280-e37ebed9d7d7");
-
   return (
     <div>
       <div className="container mt-5">
         <h2 className="mb-5 text-6xl font-semibold">Your Lists</h2>
 
         <Separator className="bg-textMain" />
-        {data.map((item) => {
+        {/* {data.map((item) => {
           return <div key={item.movieId}>{item.movieId}</div>;
-        })}
+        })} */}
       </div>
     </div>
   );
